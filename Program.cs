6643ac051b1d88a40a4cf9526f88ff548e08d6a6
Igor Biletskyi <zipski@gmail.com>
2024-02-29 09:37:50 +0100

    code uploaded

--- Program.cs
+++ Program.cs
@@ -5,26 +5,26 @@ class Program
     static void Main()
     {
         // задаем массив строк с названиями брендов
-        string[] carBrands = { "BMW", "Audi", "VW", 'VAZ', "Ford", "Fiat", "GM", "Kia", 'LUAZ'};
+        string[] carBrands = { "BMW", "Audi", "VW", "Ferrari",};
 
         // создаем новый массив для хранения результатов
         string[] result = new string[carBrands.Length];
-        int realSize = 0;
+        int position = 0;
 
         // ищем в массиве по длине
         foreach (var brand in carBrands)
         {
             if (brand.Length <= 3)
             {
-                result[realSize++] = brand; // добавляем марки в новый массив        
+                result[position++] = brand; // добавляем марки в новый массив        
             }
         }
 
         // Вывод берндов автомобилей
         Console.WriteLine("Бренды автомобилей с названиями до 3 символов:");
-        for (int i = 0; i < realSize; i++)
+        for (int i = 0; i < position; i++)
         {
-            Console
+            Console.WriteLine(result[i]);
         }
     }
 }
